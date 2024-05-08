7626d5c38963031fc5e3faef1691748c9dfddf27
Srihariva1111 <sriharivas5@gmail.com>
2024-05-08 19:24:47 +0530

    push changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,15 +9,8 @@ import Flex2 from './Pages/Home/Css/Flex/Flex2/Flex2'
 import Flex3 from './Pages/Home/Css/Flex/Flex3/Flex3'
 import Flex4 from './Pages/Home/Css/Flex/Flex4/Flex4'
 import Flex5 from './Pages/Home/Css/Flex/Flex5/Flex5'
-// <<<<<<< HEAD
 import FlexIntro from './Pages/Home/Css/Flex/FlexIntro/FlexIntro'
-// =======
 import Level1 from './Pages/Home/Javascript/Basics/Level1/Level1'
-<<<<<<< HEAD
-// >>>>>>> 028ae775f3a69843688c05b6f023e1e17cc76c4c
-=======
-import Transform1 from './Pages/Home/Css/Transform/Transform1/Transform1'
->>>>>>> a8cb3ea4800c9653311406afc6b4d7be657a5f59
 
 const App = () => {
   return (
@@ -33,12 +26,7 @@ const App = () => {
           <Route path='/Smart-Learning/Css/Flex/Flex3' element={<Flex3/>} />
           <Route path='/Smart-Learning/Css/Flex/Flex4' element={<Flex4/>} />
           <Route path='/Smart-Learning/Css/Flex/Flex5' element={<Flex5/>} />
-<<<<<<< HEAD
           <Route path='/Smart-Learning/Css/Flex/FlexIntro' element={<FlexIntro/>} />
-=======
-          <Route path='/Smart-Learning/Css/Transform/Transform1' element={<Transform1/>} />
-          
->>>>>>> a8cb3ea4800c9653311406afc6b4d7be657a5f59
         </Routes>
       </BrowserRouter>
     </div>
