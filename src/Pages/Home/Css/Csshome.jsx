c8fb404be73bec598c84217d1e853f82636dee7f
Srihariva1111 <sriharivas5@gmail.com>
2024-05-14 07:13:17 +0530

    push changes

--- src/Pages/Home/Css/Csshome.jsx
+++ src/Pages/Home/Css/Csshome.jsx
@@ -6,6 +6,7 @@ const Csshome = () => {
     <div className='cssHome'>
         <Link to='/Smart-Learning/Css/Flex/FlexIntro'><h1>Flex</h1></Link>
         <Link to='/Smart-Learning/Css/Transform/Transform1'><h1>Transform</h1></Link>
+        <Link to='/Smart-Learning/Css/Selectors/Selectors1'><h1>Selectors</h1></Link>
 
     </div>
   )
