d740ab091e0d622a6d27dd430ac0281cdd63ad91
ravikumar197 <68100532+ravikumar197@users.noreply.github.com>
2024-05-19 09:00:35 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,32 @@
-import React from 'react'
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Home from './Pages/Home/Home'
-import Csshome from './Pages/Home/Css/Csshome'
-import Javascript from './Pages/Home/Javascript/Javascript'
-import FlexIntro from './Pages/Home/Css/Flex/FlexIntro/FlexIntro'
-import Flex from './Pages/Home/Css/Flex/Flex1/Flex'
-import Flex2 from './Pages/Home/Css/Flex/Flex2/Flex2'
-import Flex3 from './Pages/Home/Css/Flex/Flex3/Flex3'
-import Flex4 from './Pages/Home/Css/Flex/Flex4/Flex4'
-import Flex5 from './Pages/Home/Css/Flex/Flex5/Flex5'
-
-import While from '../src/Pages/Home/Javascript/Loops/WhileLoop/While'
-import If_Condition from '../src/Pages/Home/Javascript/Conditional_Statements/If/If_Condition'
-
-import FlexIntro from './Pages/Home/Css/Flex/FlexIntro/FlexIntro'
-
-import Flex6 from './Pages/Home/Css/Flex/Flex6/Flex6'
-import Flex7 from './Pages/Home/Css/Flex/Flex7/Flex7'
-import Flex8 from './Pages/Home/Css/Flex/Flex8/Flex8'
-import Selectors from './Pages/Home/Css/Selectors/Selector1/Selectors'
-import Selectors2 from './Pages/Home/Css/Selectors/Selectors2/Selectors2'
-import Selectors3 from './Pages/Home/Css/Selectors/Selectors3/Selectors3'
-import Selectors4 from './Pages/Home/Css/Selectors/Selectors4/Selectors4'
-import Level1 from './Pages/Home/Javascript/Basics/Level1/Level1'
-import Level2 from './Pages/Home/Javascript/Basics/Level2/Level2'
-import Level3 from './Pages/Home/Javascript/Basics/Level3/Level3'
-
-import Javascript_index from './Pages/Home/Javascript/Javascript_index/Javascript_index'
-import For_Loop from './Pages/Home/Javascript/Loops/ForLoop/For_Loop'
-
-
-import Transform1 from './Pages/Home/Css/Transform/Transform1/Transform1'
-import Navbar from './Components/Navbar/Navbar'
-import Positions from './Pages/Home/Css/Positions/Positions'
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home/Home';
+import Csshome from './Pages/Home/Css/Csshome';
+import Javascript from './Pages/Home/Javascript/Javascript';
+import FlexIntro from './Pages/Home/Css/Flex/FlexIntro/FlexIntro';
+import Flex from './Pages/Home/Css/Flex/Flex1/Flex';
+import Flex2 from './Pages/Home/Css/Flex/Flex2/Flex2';
+import Flex3 from './Pages/Home/Css/Flex/Flex3/Flex3';
+import Flex4 from './Pages/Home/Css/Flex/Flex4/Flex4';
+import Flex5 from './Pages/Home/Css/Flex/Flex5/Flex5';
+import While from './Pages/Home/Javascript/Loops/WhileLoop/While';
+import If_Condition from './Pages/Home/Javascript/Conditional_Statements/If/If_Condition';
+import Flex6 from './Pages/Home/Css/Flex/Flex6/Flex6';
+import Flex7 from './Pages/Home/Css/Flex/Flex7/Flex7';
+import Flex8 from './Pages/Home/Css/Flex/Flex8/Flex8';
+import Selectors from './Pages/Home/Css/Selectors/Selector1/Selectors';
+import Selectors2 from './Pages/Home/Css/Selectors/Selectors2/Selectors2';
+import Selectors3 from './Pages/Home/Css/Selectors/Selectors3/Selectors3';
+import Selectors4 from './Pages/Home/Css/Selectors/Selectors4/Selectors4';
+import Level1 from './Pages/Home/Javascript/Basics/Level1/Level1';
+import Level2 from './Pages/Home/Javascript/Basics/Level2/Level2';
+import Level3 from './Pages/Home/Javascript/Basics/Level3/Level3';
+import Javascript_index from './Pages/Home/Javascript/Javascript_index/Javascript_index';
+import For_Loop from './Pages/Home/Javascript/Loops/ForLoop/For_Loop';
+import Transform1 from './Pages/Home/Css/Transform/Transform1/Transform1';
+import Navbar from './Components/Navbar/Navbar';
+import Positions from './Pages/Home/Css/Positions/Positions';
 
 
 
