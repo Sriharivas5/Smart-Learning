5ec45a6d3f228cc2da739a9a54985123ed1c548a
ravikumar197 <68100532+ravikumar197@users.noreply.github.com>
2024-05-21 11:32:49 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,25 +2,25 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Pages/Home/Home";
-import Csshome from "./Pages/Home/Css/Csshome";
-import Javascript from "./Pages/Home/Javascript/Javascript";
-import FlexIntro from "./Pages/Home/Css/Flex/FlexIntro/FlexIntro";
-import Flex from "./Pages/Home/Css/Flex/Flex1/Flex";
-import Flex2 from "./Pages/Home/Css/Flex/Flex2/Flex2";
-import Flex3 from "./Pages/Home/Css/Flex/Flex3/Flex3";
-import Flex4 from "./Pages/Home/Css/Flex/Flex4/Flex4";
-import Flex5 from "./Pages/Home/Css/Flex/Flex5/Flex5";
-import Flex6 from "./Pages/Home/Css/Flex/Flex6/Flex6";
-import Flex7 from "./Pages/Home/Css/Flex/Flex7/Flex7";
-import Flex8 from "./Pages/Home/Css/Flex/Flex8/Flex8";
-import Selectors from "./Pages/Home/Css/Selectors/Selector1/Selectors";
-import Selectors2 from "./Pages/Home/Css/Selectors/Selectors2/Selectors2";
-import Selectors3 from "./Pages/Home/Css/Selectors/Selectors3/Selectors3";
-import Selectors4 from "./Pages/Home/Css/Selectors/Selectors4/Selectors4";
-import Level1 from "./Pages/Home/Javascript/Basics/Level1/Level1";
-import Level2 from "./Pages/Home/Javascript/Basics/Level2/Level2";
-import Level3 from "./Pages/Home/Javascript/Basics/Level3/Level3";
+// import Home from "./Pages/Home/Home";
+// import Csshome from "./Pages/Home/Css/Csshome";
+// import Javascript from "./Pages/Home/Javascript/Javascript";
+// import FlexIntro from "./Pages/Home/Css/Flex/FlexIntro/FlexIntro";
+// import Flex from "./Pages/Home/Css/Flex/Flex1/Flex";
+// import Flex2 from "./Pages/Home/Css/Flex/Flex2/Flex2";
+// import Flex3 from "./Pages/Home/Css/Flex/Flex3/Flex3";
+// import Flex4 from "./Pages/Home/Css/Flex/Flex4/Flex4";
+// import Flex5 from "./Pages/Home/Css/Flex/Flex5/Flex5";
+// import Flex6 from "./Pages/Home/Css/Flex/Flex6/Flex6";
+// import Flex7 from "./Pages/Home/Css/Flex/Flex7/Flex7";
+// import Flex8 from "./Pages/Home/Css/Flex/Flex8/Flex8";
+// import Selectors from "./Pages/Home/Css/Selectors/Selector1/Selectors";
+// import Selectors2 from "./Pages/Home/Css/Selectors/Selectors2/Selectors2";
+// import Selectors3 from "./Pages/Home/Css/Selectors/Selectors3/Selectors3";
+// import Selectors4 from "./Pages/Home/Css/Selectors/Selectors4/Selectors4";
+// import Level1 from "./Pages/Home/Javascript/Basics/Level1/Level1";
+// import Level2 from "./Pages/Home/Javascript/Basics/Level2/Level2";
+// import Level3 from "./Pages/Home/Javascript/Basics/Level3/Level3";
 // import React from "react";
 // import { BrowserRouter, Routes, Route } from "react-router-dom";
 // import Home from "./Pages/Home/Home";
@@ -43,9 +43,9 @@ import Level3 from "./Pages/Home/Javascript/Basics/Level3/Level3";
 // import Level2 from "./Pages/Home/Javascript/Basics/Level2/Level2";
 // import Level3 from "./Pages/Home/Javascript/Basics/Level3/Level3";
 
-import React from 'react';
-import "./App.css";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import React from 'react';
+// import "./App.css";
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home/Home';
 import Csshome from './Pages/Home/Css/Csshome';
 import Javascript from './Pages/Home/Javascript/Javascript';
@@ -88,7 +88,7 @@ const App = () => {
         <Navbar />
 
         <Routes>
-          <Route path="/Smart-Learning" element={<Home />} />
+          {/* <Route path="/Smart-Learning" element={<Home />} />
           <Route path="/Smart-Learning/Css" element={<Csshome />} />
           <Route path="/Smart-Learning/Js" element={<Javascript />} />
           <Route path="/Smart-Learning/Js/Basics/Level1" element={<Level1 />} />
@@ -148,7 +148,7 @@ const App = () => {
           <Route
             path="/Smart-Learning/Css/Positions/Positions2"
             element={<Position2 />}
-          />
+          /> */}
 {/* ======= */}
 
           <Route path='/Smart-Learning' element={<Home/>} />
--- src/Pages/Home/Javascript/Conditional_Statements/Else/Else.jsx
+++ src/Pages/Home/Javascript/Conditional_Statements/Else/Else.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
-
+import '../Else/Else.css' 
 const Else = () => {
   return (
-    <div>Else</div>
+    <div className="else_main">
+        <div className="else_left"></div>
+        <div className="else_right"></div>
+    </div>
   )
 }
 
